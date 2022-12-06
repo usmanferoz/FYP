c0680674022d661fbc3b944f6c6dbc93c204b6d7
usmanferoz <usman_feroz786@hotmail.com>
2022-12-06 22:29:54 +0500

    cors headers updated

--- FMS/settings.py
+++ FMS/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'common',
     'vehicle',
     'jobs',
+    'corsheaders',
     
     'rest_framework',
     'rest_framework.authtoken',
@@ -82,6 +83,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
