b29af070bc9fb068c374a997ccd34fbfb855640c
usmanferoz <usman_feroz786@hotmail.com>
2022-12-15 10:41:19 +0500

    updated

--- FMS/settings.py
+++ FMS/settings.py
@@ -27,10 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-
-
-
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = [
--- vehicle/serializer.py
+++ vehicle/serializer.py
@@ -22,5 +22,8 @@ class VehicleAllocationSerializer(ModelSerializer):
         data['vehicle_number'] = instance.vehicle.vehicle_number
         data['emoloyee_email'] = instance.employee.email
         data['empolyee_type'] = instance.employee.user_type
+        data['empolyee_id'] = instance.employee.id
+        data['empolyee_id'] = instance.employee.id
+        data['employee_name'] = instance.employee.first_name
 
         return data
